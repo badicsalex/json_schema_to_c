daefb550a0470bfc52cad37b2f538ddf3850da81
Alex Badics <admin@stickman.hu>
2020-01-31 21:09:10 +0100

    dont declare typedefs for simple types

--- json_schema_t_c.py
+++ json_schema_t_c.py
@@ -6,8 +6,7 @@ from collections import namedtuple
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description="Create a JSON parser in C based on a json schema")
+    parser = argparse.ArgumentParser(description="Create a JSON parser in C based on a json schema")
     parser.add_argument("schema_file", type=argparse.FileType('r'))
     parser.add_argument("c_file", type=argparse.FileType('w'))
     parser.add_argument("h_file", type=argparse.FileType('w'))
@@ -17,81 +16,104 @@ def parse_args():
 class Generator(ABC):
     @classmethod
     @abstractmethod
+    def generate_field_declaration(cls, schema, name, field_name, out_file):
+        pass
+
+    @classmethod
     def generate_type_declaration(cls, schema, name, out_file):
         pass
 
 
 class StringGenerator(Generator):
     @classmethod
-    def generate_type_declaration(cls, schema, name, out_file):
+    def generate_field_declaration(cls, schema, name, field_name, out_file):
         if "maxLength" not in schema:
             raise ValueError("Strings must have maxLength")
-        out_file.write("typedef char {}_t[{}];\n\n".format(
-            name, schema["maxLength"] + 1))
+        out_file.write("    char {}[{}];\n".format(field_name, schema["maxLength"] + 1))
 
 
 class NumberGenerator(Generator):
     @classmethod
-    def generate_type_declaration(cls, schema, name, out_file):
-        out_file.write("typedef int64_t {}_t;\n\n".format(name))
+    def generate_field_declaration(cls, schema, name, field_name, out_file):
+        out_file.write("    int64_t {};\n".format(field_name))
 
 
 class BoolGenerator(Generator):
     @classmethod
-    def generate_type_declaration(cls, schema, name, out_file):
-        out_file.write("typedef bool {}_t;\n\n".format(name))
+    def generate_field_declaration(cls, schema, name, field_name, out_file):
+        out_file.write("    bool {};\n".format(field_name))
 
 
 class ObjectGenerator(Generator):
+    @classmethod
+    def generate_field_declaration(cls, schema, name, field_name, out_file):
+        out_file.write("    {}_t {};\n".format(name, field_name))
+
     @classmethod
     def generate_type_declaration(cls, schema, name, out_file):
         if "additionalProperties" not in schema or schema["additionalProperties"] != False:
             raise ValueError(
                 "Object types must have additionalProperties set to false")
         for prop_name, prop_schema in schema["properties"].items():
-            generate_type_declaration(
-                prop_schema, "{}_{}".format(name, prop_name), out_file)
+            GlobalGenerator.generate_type_declaration(prop_schema, "{}_{}".format(name, prop_name), out_file)
 
         out_file.write("typedef struct {}_s ".format(name) + "{\n")
-        for prop_name in schema["properties"]:
-            out_file.write("    {n}_{p}_t {p};\n".format(n=name, p=prop_name))
+        for prop_name, prop_schema in schema["properties"].items():
+            GlobalGenerator.generate_field_declaration(
+                prop_schema,
+                "{}_{}".format(name, prop_name),
+                prop_name,
+                out_file
+            )
         out_file.write("}} {}_t;\n\n".format(name))
 
 
 class ArrayGenerator(Generator):
     @classmethod
+    def generate_field_declaration(cls, schema, name, field_name, out_file):
+        out_file.write("    {}_t {};\n".format(name, field_name))
+
+    @classmethod
     def generate_type_declaration(cls, schema, name, out_file):
         if "maxItems" not in schema:
             raise ValueError("Arrays must have maxItems")
-        generate_type_declaration(
-            schema["items"], "{}_item".format(name), out_file)
+        GlobalGenerator.generate_type_declaration(schema["items"], "{}_item".format(name), out_file)
 
         out_file.write("typedef struct {}_s ".format(name) + "{\n")
         out_file.write("    uint64_t n;\n")
-        out_file.write("    {}_item_t items[{}];\n".format(
-            name, schema["maxItems"]))
+        GlobalGenerator.generate_field_declaration(
+            schema["items"],
+            "{}_item".format(name),
+            "items[{}]".format(schema["maxItems"]), out_file
+        )
         out_file.write("}} {}_t;\n\n".format(name))
 
 
-generators = {
-    "string": StringGenerator,
-    "integer": NumberGenerator,
-    "boolean": BoolGenerator,
-    "object": ObjectGenerator,
-    "array": ArrayGenerator,
-}
+class GlobalGenerator(Generator):
+    OTHER_GENERATORS = {
+        "string": StringGenerator,
+        "integer": NumberGenerator,
+        "boolean": BoolGenerator,
+        "object": ObjectGenerator,
+        "array": ArrayGenerator,
+    }
 
+    @classmethod
+    def generate_field_declaration(cls, schema, name, field_name, out_file=''):
+        cls.OTHER_GENERATORS[schema["type"]]\
+            .generate_field_declaration(schema, name, field_name, out_file)
 
-def generate_type_declaration(schema, name, out_file=''):
-    generators[schema["type"]].generate_type_declaration(
-        schema, name, out_file)
+    @classmethod
+    def generate_type_declaration(cls, schema, name, out_file=''):
+        cls.OTHER_GENERATORS[schema["type"]]\
+            .generate_type_declaration(schema, name, out_file)
 
 
 def main(args):
     schema = json.load(args.schema_file)
     args.h_file.write("#include <stdint.h>\n")
     args.h_file.write("#include <stdbool.h>\n\n")
-    generate_type_declaration(schema, "root", args.h_file)
+    GlobalGenerator.generate_type_declaration(schema, "root", args.h_file)
 
 
 if __name__ == "__main__":
