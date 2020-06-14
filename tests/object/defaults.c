d7b1c6ee98c85ac508c983019ddde0d19ec27e30
Alex Badics <admin@stickman.hu>
2020-06-14 21:29:33 +0200

    tests/object/defaults: remove undefined behaviour
    It actually failed with clang.

--- tests/object/defaults.c
+++ tests/object/defaults.c
@@ -9,35 +9,23 @@
 int main(int argc, char** argv){
     (void)argc;
     (void)argv;
-    root_t got, expected;
-    /* memset is needed for the later memcmp. Otherwise, padding bytes will not get set to 0. */
-    memset(&got, 0, sizeof(root_t));
-    memset(&expected, 0, sizeof(root_t));
+    root_t got;
 
-    /* Now that padding bytes are take care of, set fields */
-    expected = (root_t){
-        .name="cauliflower",
-        .is_good=true,
-        .number=1337,
-        .width=567.12345,
-        .id="1234",
-        .exists=false,
-        .mass=4321,
-        .attributes={.n=0},
-        .sub_obj={
-            .is_good=true,
-            .number=1337,
-            .sub_obj={
-                .is_good=true,
-                .number=1337
-            }
-        },
-        .the_enum=ROOT_THE_ENUM_ENUM_VAL_2
-    };
     assert(!json_parse_root("{ \"id\": \"1234\", \"exists\": false, \"mass\": 4321}", &got));
 
-    /* This is actually UB because of the padding bytes. We should compare field-by-field */
-    assert(!memcmp(&got, &expected, sizeof(root_t)));
+    assert(strcmp(got.name, "cauliflower") == 0);
+    assert(got.is_good == true);
+    assert(got.number == 1337);
+    assert(got.width == 567.12345);
+    assert(strcmp(got.id, "1234") == 0);
+    assert(got.exists == false);
+    assert(got.mass == 4321);
+    assert(got.attributes.n == 0);
+    assert(got.sub_obj.is_good == true);
+    assert(got.sub_obj.number == 1337);
+    assert(got.sub_obj.sub_obj.is_good == true);
+    assert(got.sub_obj.sub_obj.number == 1337);
+    assert(got.the_enum == ROOT_THE_ENUM_ENUM_VAL_2);
 
     assert(
         !json_parse_root(
