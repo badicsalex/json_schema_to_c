fc113522aa0a71285704c668fa76bb4795084ec0
Alex Badics <admin@stickman.hu>
2020-08-24 22:40:51 +0200

    tests: modified custom_parser test to check sub-objects too
    The issues in the last two commits are now checked with this test.

--- tests/other/custom_parser.c
+++ tests/other/custom_parser.c
@@ -11,21 +11,30 @@ int main(int argc, char** argv){
     (void)argv;
     root_t root = {};
     assert(!json_parse_root("{}", &root));
-    assert(root.id == 0x01EFCDAB01EFCDAB);
+    assert(root.ids.id == 0x01EFCDAB01EFCDAB);
+    assert(root.ids.short_id == 0xEFCDAB0000000000);
     assert(root.point.x == 'a');
     assert(root.point.y == 'b');
 
-    assert(!json_parse_root("{\"id\": \"1234\", \"short_id\": \"1234\", \"point\": \"xy\"}", &root));
-    assert(root.id == 0x3412000000000000);
-    assert(root.short_id == 0x3412000000000000);
+    assert(!json_parse_root("{\"ids\": { \"id\": \"1234\", \"short_id\": \"1234\"}, \"point\": \"xy\"}", &root));
+    assert(root.ids.id == 0x3412000000000000);
+    assert(root.ids.short_id == 0x3412000000000000);
     assert(root.point.x == 'x');
     assert(root.point.y == 'y');
 
-    assert(json_parse_root("{\"id\": \"X\", \"point\": \"xy\"}", &root));
-    assert(json_parse_root("{\"id\": \"1\", \"point\": \"xyz\"}", &root));
+    assert(!json_parse_root("{\"ids\": {\"short_id\": \"1234\"}, \"point\": \"xy\"}", &root));
+    assert(root.ids.id == 0x01EFCDAB01EFCDAB);
+    assert(root.ids.short_id == 0x3412000000000000);
 
-    assert(json_parse_root("{\"short_id\": \"1\"}", &root));
-    assert(json_parse_root("{\"short_id\": \"12345678\"}", &root));
-    assert(json_parse_root("{\"short_id\": 12345678}", &root));
+    assert(!json_parse_root("{\"ids\": {\"id\": \"1234\"}, \"point\": \"xy\"}", &root));
+    assert(root.ids.id == 0x3412000000000000);
+    assert(root.ids.short_id == 0xEFCDAB0000000000);
+
+    assert(json_parse_root("{\"ids\": {\"id\": \"X\"}, \"point\": \"xy\"}", &root));
+    assert(json_parse_root("{\"ids\": {\"id\": \"1\"}, \"point\": \"xyz\"}", &root));
+
+    assert(json_parse_root("{\"ids\": {\"short_id\": \"1\"}}", &root));
+    assert(json_parse_root("{\"ids\": {\"short_id\": \"12345678\"}}", &root));
+    assert(json_parse_root("{\"ids\": {\"short_id\": 12345678}}", &root));
     return 0;
 }
