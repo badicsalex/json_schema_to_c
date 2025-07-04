cfb649b79fac03ec03f578a50bb02ab2b3c82694
Clément Saccoccio <clement.saccoccio@maia-space.com>
2025-07-04 16:57:53 +0200

    codegen/integer: added support for "js2cParseFunction" on integers

--- js2c/codegen/integer.py
+++ js2c/codegen/integer.py
@@ -35,7 +35,7 @@ class IntegerType(CType):
 
     def __init__(self, generator, type_name, description):
         if type_name not in self.SIGNED_TYPES + self.UNSIGNED_TYPES:
-            raise SchemaError(generator, "Unsupported integer type: {}".format(type_name))
+            raise SchemaError(generator, "Unsupported integer type: " + type_name)
         super().__init__(type_name, description)
 
     def is_unsigned(self):
@@ -49,6 +49,8 @@ class IntegerGeneratorBase(Generator):
         "exclusiveMinimum",
         "exclusiveMaximum",
         "default",
+        "js2cParseFunction",
+        "js2cParseType",
     )
 
     minimum = None
@@ -56,6 +58,8 @@ class IntegerGeneratorBase(Generator):
     exclusiveMinimum = None
     exclusiveMaximum = None
     default = None
+    js2cParseFunction = None
+    js2cParseType = None
 
     def __init__(self, schema, parameters):
         super().__init__(schema, parameters)
@@ -67,8 +71,14 @@ class IntegerGeneratorBase(Generator):
             else:
                 c_type_name = "int64_t"
 
-        self.c_type = IntegerType(self, c_type_name, self.description)
-        if self.c_type.is_unsigned():
+        if self.js2cParseFunction is not None:
+            if self.js2cParseType is None: self.js2cParseType = "int64_t"
+            self.c_intermediary_type = CType(self.js2cParseType, self.description)
+            self.c_type = CType(c_type_name, self.description)
+        else:
+            self.c_type = self.c_intermediary_type = IntegerType(self, c_type_name, self.description)
+
+        if (self.js2cParseFunction is not None and self.minimum >= 0) or self.c_intermediary_type.is_unsigned():
             self.parser_fn = "builtin_parse_unsigned"
             self.parsed_type = "uint64_t"
             self.parsed_type_printf_macro = "PRIu64"
@@ -96,7 +106,7 @@ class IntegerGeneratorBase(Generator):
         # pylint: disable=too-many-arguments
         if check_number is None:
             return
-        with out_file.if_block("int_parse_tmp {} ({}) {}{}".format(inverted_check_operator, self.c_type, check_number, self.default_suffix)):
+        with out_file.if_block(f"int_parse_tmp {inverted_check_operator} ({self.c_intermediary_type}) {check_number}{self.default_suffix}"):
             # Roll back the token, as the value was not actually correct
             out_file.print("parse_state->current_token -= 1;")
             self.generate_logged_error(
@@ -110,8 +120,19 @@ class IntegerGeneratorBase(Generator):
                 exit_statement=on_err
             )
 
+    def generate_custom_parser_call(self, in_value, out_var_name, out_file, on_err):
+        out_file.print("const char *error = NULL;")
+        out_file.require_section(self.js2cParseFunction)
+        with out_file.if_block(f"{self.js2cParseFunction}({in_value}, {out_var_name}, &error)"):
+            self.generate_logged_error([
+                f"Error parsing '%s', value=\\\"%\" {self.parsed_type_printf_macro} \"\\\": %s",
+                "parse_state->current_key",
+                in_value,
+                f'error != NULL ? error : "error calling {self.js2cParseFunction}"',
+            ], out_file, exit_statement=on_err)
+
     def generate_parser_call(self, out_var_name, out_file, on_err="return true;"):
-        out_file.print("{} int_parse_tmp = 0;".format(self.parsed_type))
+        out_file.print(f"{self.parsed_type} int_parse_tmp = 0;")
         out_file.require_section(self.parser_fn)
         parser_call = "{}(parse_state, {}, {}, {}, &int_parse_tmp)".format(
             self.parser_fn,
@@ -125,7 +146,10 @@ class IntegerGeneratorBase(Generator):
         self.generate_range_check(self.maximum, self.parsed_type_printf_macro, "<=", ">", out_file, on_err)
         self.generate_range_check(self.exclusiveMinimum, self.parsed_type_printf_macro, ">", "<=", out_file, on_err)
         self.generate_range_check(self.exclusiveMaximum, self.parsed_type_printf_macro, "<", ">=", out_file, on_err)
-        out_file.print("*{} = int_parse_tmp;".format(out_var_name))
+        if self.js2cParseFunction:
+            self.generate_custom_parser_call("int_parse_tmp", out_var_name, out_file, on_err)
+        else:
+            out_file.print(f"*{out_var_name} = int_parse_tmp;")
 
     def has_default_value(self):
         return super().has_default_value() or self.default is not None
@@ -133,7 +157,11 @@ class IntegerGeneratorBase(Generator):
     def generate_set_default_value(self, out_var_name, out_file):
         if super().generate_set_default_value(out_var_name, out_file):
             return
-        out_file.print(f"{out_var_name} = ({self.c_type}) {self.default}{self.default_suffix};")
+        default_value = f"({self.c_intermediary_type}) {self.default}{self.default_suffix}"
+        if self.js2cParseFunction:
+            self.generate_custom_parser_call(default_value, "&" + out_var_name, out_file, on_err="return true;")
+        else:
+            out_file.print(f"{out_var_name} = {default_value};")
 
 
 class IntegerGenerator(IntegerGeneratorBase):
@@ -180,11 +208,7 @@ class NumericStringGenerator(IntegerGenerator):
 
         if self.pattern not in pattern_set:
             valid_patterns = ", ".join('"{}"'.format(p) for p in pattern_set)
-            raise SchemaError(
-                self,
-                'Pattern "{}" is not a valid pattern for this value range. Valid patterns are: {}'
-                .format(self.pattern, valid_patterns)
-            )
+            raise SchemaError(self, f'Pattern "{self.pattern}" is not a valid pattern for this value range. Valid patterns are: {valid_patterns}')
         self.radix = pattern_set[self.pattern]
         if isinstance(self.default, str):
             self.default = int(self.default, self.radix)
